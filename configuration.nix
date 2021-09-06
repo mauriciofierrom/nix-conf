20794d89d39942e7d6eb2ce961c7bb3eb33f817b
Mauricio Fierro <mauriciofierrom@gmail.com>
2021-09-06 23:14:26 +0000

    Set neovim as default editor

--- configuration.nix
+++ configuration.nix
@@ -105,6 +105,7 @@ in
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
+  environment.variables.EDITOR = "nvim";
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     cachix
