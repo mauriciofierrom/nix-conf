be1ffe47afdc5e72626ff50b493531458246aa3a
Mauricio Fierro <mauriciofierrom@gmail.com>
2026-08-04 04:30:03 -0500

    Add zw3rk cache for haskell.nix
    And add the trusted key as well

--- configuration.nix
+++ configuration.nix
@@ -23,9 +23,11 @@ in
   # Binary Cache for Haskell.nix
   nix.settings.trusted-public-keys = [
     "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+    "loony-tools:pr9m4BkM/5/eSTZlkQyRt57Jz7OMBxNSUiMC4FkcNfk="
   ];
   nix.settings.substituters = [
     "https://cache.iog.io"
+    "https://cache.zw3rk.com"
   ];
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nixpkgs.config.allowUnfree = true;
